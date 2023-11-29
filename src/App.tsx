f3df1082f8ac6dd5770f6ba831be3f7b2cea2043
Mikhail-Mishchukov <mi.mishchukov@gmail.com>
2023-11-29 23:03:45 +0500

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { store } from 'Store/Store';
 import 'UI/Styles/Reset.scss';
+import 'UI/Styles/GlobalStyles.scss';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { MainPage } from './Pages/MainPage/MainPage';
