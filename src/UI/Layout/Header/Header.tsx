6721bdcb932a327872f7d378d0d1343d662ce62e
Mikhail-Mishchukov <mi.mishchukov@gmail.com>
2023-12-10 23:55:30 +0500

    Develop Header component

--- src/UI/Layout/Header/Header.tsx
+++ src/UI/Layout/Header/Header.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import { Icon } from 'UI/Icon';
+import { Logo } from './Components/Logo';
 import classes from './Header.module.scss';
 
 export function Header() {
-    return <div className={classes.header}>Header</div>;
+    return (
+        <header className={classes.header}>
+            <Link to="/">
+                <Logo />
+            </Link>
+            <Link to="/">Home</Link>
+            <Link to="/aboutDeveloper">
+                <div>About developer</div>
+            </Link>
+            <Link to="/">
+                <Icon type="logout" size="big" />
+            </Link>
+        </header>
+    );
 }
